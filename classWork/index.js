959e61491681edbe8cab6e4bf6d78a90f31e2a18
Ritu Singh <69470113+RituSingh2002@users.noreply.github.com>
2022-07-29 17:09:17 +0530

    ggg

---
+++ classWork/index.js
@@ -0,0 +1,97 @@
+const fs = require('express');
+
+const express = require('express');
+
+const cors = require('cors');
+
+const _ = require('lodash');
+
+const { v4: uuid } = require("uuid");
+
+
+
+const app = express();
+
+
+
+app.use(express.json());
+
+app.use(express.cors());
+
+
+
+app.get("/outfit", (req, res) => {
+
+const shirts = ["white", "blue"];
+
+const jeans = ["grey", "green"];
+
+const shoes = ["brown", "black"];
+
+
+
+res.json(({
+
+shirt: _.sample(shirts),
+
+jeans: _.sample(jeans),
+
+shoes: _.sample(shoes)
+
+}));
+
+});
+
+
+
+app.post("/comments/:id", async (req, res) => {
+
+const id = req.params.id;
+
+let content;
+
+try {
+
+content = await fs.readFile('data/comments/${id}.txt', "utf-8");
+
+}
+
+catch (err)
+
+{
+
+return res.sendStatus(404);
+
+
+
+}
+
+res.json({ content: content });
+
+}
+
+)
+
+app.post("comments", async (req, res) => {
+
+const id = uuid();
+
+const content = req.body.content;
+
+if (!content) {
+
+return res.sendStatus(400);
+
+}
+
+await fs.mkdir(data / customElements, { recursive: true });
+
+await fs.writeFile('data/comments/${id}.txt', content);
+
+res.sendStatus(201).json({ id: id });
+
+});
+
+
+
+app.listen(3000, () => console.log("Api is running !"));
